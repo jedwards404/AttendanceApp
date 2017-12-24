a455418c172d8f5fb8aba6afe9e41ed17f23f019
Joshua Edwards <jetank@earthlink.net>
2017-12-23 22:30:55 -0600

    fifth change to output message "welcome message"

--- src/edu/xavier/cpsc2730/Main.java
+++ src/edu/xavier/cpsc2730/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, and welcome to my new Attendance App! stuff");
+        System.out.println("Hello, and welcome to my new Attendance App!");
     }
 }
