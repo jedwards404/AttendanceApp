b55e4fd19762a6bd84afb2502dd714592f3c4675
Joshua Edwards <jetank@earthlink.net>
2017-12-23 21:44:49 -0600

    third change to output message "version control test" + "test"

--- src/edu/xavier/cpsc2730/Main.java
+++ src/edu/xavier/cpsc2730/Main.java
@@ -3,6 +3,6 @@ package edu.xavier.cpsc2730;
 public class Main {
 
     public static void main(String[] args) {
-System.out.println("Version control test");
+        System.out.println("Version cortrol test" + "test");
     }
 }
