a348b51655bb43b48e63d30db952f875e1fb4184
Joshua Edwards <jetank@earthlink.net>
2017-12-24 23:05:08 -0600

    #153805996

--- src/edu/xavier/cpsc2730/Main.java
+++ src/edu/xavier/cpsc2730/Main.java
@@ -15,12 +15,19 @@ public class Main {
         System.out.println("\n\nHello, and welcome " + username + " to my new Attendance App!\n");
 
         //create ArrayList to find absences and put random numbers in an elements
+        ArrayList<Integer> attended = Initialize(username);
+        System.out.println("The elements are " + attended);
+
+        // how many students have perfect att
+    }
+
+    private static ArrayList<Integer> Initialize(String username) {
         Random rand = new Random();
-        ArrayList<Integer> attended = new ArrayList<>();
+        ArrayList<Integer> absenses = new ArrayList<>();
         for (int i = 0; i < username.length(); i++) {
             int genrandom = rand.nextInt(11);
-            attended.add(genrandom);
+            absenses.add(genrandom);
         }
-        System.out.println("The elements are " + attended);
+        return absenses;
     }
 }
